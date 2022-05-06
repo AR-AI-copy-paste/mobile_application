92769e4894cf666d3d25763dde113e1d31d190a8
Aimen Sahnoun <aimen.sahnoun@lyntlabs.com>
2022-05-06 14:14:47 +0300

    🖼️ UI: Added settings page UI

--- src/screens/settings/settings.js
+++ src/screens/settings/settings.js
@@ -1,5 +1,12 @@
-//React Native
-import { StyleSheet, ScrollView, View } from "react-native";
+//React Native import
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 
 //Dependncies Import
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -7,7 +14,22 @@ import { SafeAreaView } from "react-native-safe-area-context";
 //Custom components import
 import CustomText from "../../components/CustomText/CustomText";
 
+//Assets import
+import Gallery from "../../assets/icons/images.svg";
+import SignOut from "../../assets/icons/signout.svg";
+
+//utils import
+import { supabase } from "../../utils/supabase";
+
+//Recoil import
+import { useRecoilState } from "recoil";
+import { authState } from "../../recoil/state";
+
 const Settings = ({ navigation }) => {
+
+    //Recoil State
+    const [_auth, setAuth] = useRecoilState(authState);
+
   return (
     <SafeAreaView
       style={{
@@ -26,9 +48,159 @@ const Settings = ({ navigation }) => {
             padding: 20,
           }}
         >
-          <CustomText fontSize={18} fontWeight="bold">
+          <CustomText fontSize={20} fontWeight="bold">
             Profile
           </CustomText>
+
+          {/* User profile info */}
+          <View
+            style={{
+              alignItems: "center",
+              marginBottom: 20,
+            }}
+          >
+            <Image
+              style={{
+                width: 120,
+                height: 120,
+                borderRadius: 15,
+                margin: 10,
+              }}
+              source={{
+                uri: "https://i.pravatar.cc/100",
+              }}
+            />
+
+            <CustomText fontWeight="medium">Aimen Sahnoun</CustomText>
+          </View>
+
+          {/* Settings buttons */}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+
+              width: Dimensions.get("screen").width * 0.85,
+              marginBottom: 20,
+            }}
+          >
+            {/* View Profile Button */}
+            <TouchableOpacity
+              style={{
+                height: 50,
+                width: "48%",
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: "#000",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CustomText fontWeight="medium" fontSize={18}>
+                View Profile
+              </CustomText>
+            </TouchableOpacity>
+
+            {/* Edit Profile Button */}
+            <TouchableOpacity
+              style={{
+                height: 50,
+                width: "48%",
+                borderRadius: 10,
+                borderWidth: 1,
+                backgroundColor: "#000",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CustomText color="#fff" fontSize={18} fontWeight="bold">
+                Edit Profile
+              </CustomText>
+            </TouchableOpacity>
+          </View>
+
+          {/* Settings view */}
+          <CustomText
+            fontWeight="bold"
+            style={{
+              marginBottom: 20,
+            }}
+            fontSize={20}
+          >
+            Settings ⚙️
+          </CustomText>
+
+          {/* Page tab */}
+          <TouchableOpacity
+            activeOpacity={0.8}
+            // onPress={() => navigation.push("Explore")}
+            style={{
+              width: "100%",
+              height: 150,
+              borderColor: "#e5e5e5",
+              borderWidth: 1,
+              borderRadius: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: 30,
+            }}
+          >
+            {/* Icon */}
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                backgroundColor: "#e5e5e5",
+                borderRadius: 10,
+                alignItems: "center",
+                justifyContent: "center",
+                marginRight: 20,
+              }}
+            >
+              <Gallery width={35} height={35} />
+            </View>
+
+            {/* Text */}
+            <View
+              style={{
+                flexDirection: "column",
+              }}
+            >
+              <CustomText fontSize={20} fontWeight="medium">
+                Explore
+              </CustomText>
+              <CustomText
+                style={{ width: 200, color: "#a8a8a8" }}
+                fontSize={14}
+              >
+                Find images shared by other users and implement them in your
+                projects.
+              </CustomText>
+            </View>
+          </TouchableOpacity>
+
+          {/* Logout Tab */}
+          <TouchableOpacity
+            onPress={() => {
+              supabase.auth.signOut();
+              setAuth(null);
+            }}
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <SignOut width={35} height={35} />
+            <CustomText
+              fontWeight="medium"
+              fontSize={20}
+              style={{ marginLeft: 20 }}
+            >
+              Logout
+            </CustomText>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
